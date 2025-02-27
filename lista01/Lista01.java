66fa9ac72175199e1b39c8c1e7e8c386070f1df4
Raul Anselmo D Santos. <100213928+RaulAnselmoDSantos@users.noreply.github.com>
2025-02-26 22:03:12 -0300

    Lista01 RESOLVIDA

---
+++ lista01/Lista01.java
@@ -0,0 +1,67 @@
+package lista01;
+import java.util.Scanner;
+
+public class Lista01 {
+
+    
+    public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+//        EXERCICIO 01
+//        float f;;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Entre com um valor para converter de Fahrenheit para converter para celsius:  ");
+//        f = sc.nextFloat();
+//        System.out.println("Valor em Fahrenheit: " + f);
+//        System.out.println("Valor de "+f+" F para Celsius: "+ celsiusFah.converte(f));
+
+//        EXERCICIO 02
+//        int ano;
+//        int mes;
+//        int dias;
+//        System.out.print("Entre com sua idade em anos:  ");
+//        ano = sc.nextInt();
+//        System.out.print("Entre com sua idade em meses:  ");
+//        mes = sc.nextInt();
+//        System.out.print("Entre com sua idade em dias:  ");
+//        dias = sc.nextInt();
+//        System.out.println("Sua idade em dias é: " +idade.emDias(ano, mes, dias));
+        
+//        EXERCICIO 03
+//        float premio;
+//        System.out.print("Entre com o montante do premio:  ");
+//        premio = sc.nextFloat();
+//        mega.premio(premio);
+          
+//        EXERCICIO 04
+//        float c;
+//        float i;
+//        int t;
+//        System.out.println("Calcule o valor da prestação! ");
+//        System.out.print("Valor do Capital:  ");
+//        c = sc.nextFloat();
+//        System.out.print("Valor da taxa:  ");
+//        i = sc.nextFloat();
+//        System.out.print("Valor de tempo:  ");
+//        t = sc.nextInt();
+//        System.out.println("Resultado: R$ " + Banco.juroSimples(c, i, t) );
+
+//        EXERCICIO 05
+//        int x;
+//        System.out.println("Entre com um número de até 3 digitos: ");
+//        x = sc.nextInt();
+//        System.out.println("Seu número invertido é: " + Calc.inverte(x));
+          
+//        EXERCICIO 06
+//        float num1, num2;  
+//        System.out.println("Escreva o primeiro numero: ");
+//        num1 = sc.nextFloat();
+//        System.out.println("Escreva o segundo numero: ");
+//        num2 = sc.nextFloat();
+//        System.out.printf("%.1f + %.1f = %.1f \n", num1, num2, Calc.adicao(num2, num2));
+//        System.out.printf("%.1f - %.1f = %.1f \n", num1, num2, Calc.subtracao(num2, num2));
+//        System.out.printf("%.1f * %.1f = %.1f \n", num1, num2, Calc.multiplicacao(num2, num2));
+//        System.out.printf("%.1f / %.1f = %.1f \n", num1, num2, Calc.divisao(num2, num2));
+          
+            
+    }
+}
---
+++ lista01/idade.java
@@ -0,0 +1,13 @@
+
+package lista01;
+
+/**
+ *
+ * @author Raul
+ */
+public class idade {
+    public static int emDias(int ano, int mes, int dias){
+        int idade = (ano * 365)+(mes * 30)+dias;
+        return idade;
+    };
+}
